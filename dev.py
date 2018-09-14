320ed17ce827d2561fadb47dcf166dc8453d0e8e
jordetm5 <42181868+jordetm5@users.noreply.github.com>
2018-09-14 10:21:30 -0500

    Add files via upload

---
+++ dev.py
@@ -0,0 +1,27 @@
+
+#jorge Dettle Meza Dominguez
+#13/09/2018
+#derivadas n-simas para el algoritmo de gauss
+from sympy import Symbol
+
+def nf(x):
+	n=1
+	s=x
+	while n<x:
+		s=s*n
+		n=n+1
+	return s
+def P(x,n):
+	return x/((2**n)*nf(n))
+
+n=int(input("introduzca el grado del polinomio : "))
+
+r=float(input("introduzca el valor a evaluar : "))
+
+x=Symbol("x")
+y=((x**2)-1)**n
+for i in range (0,n):
+	y=y.diff(x)
+
+y=float(y.subs(x,r))
+
